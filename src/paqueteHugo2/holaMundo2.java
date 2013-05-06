2f39daf1ebe48266143716bb2ea2a91725fbf051
Administrador <Administrador@192.168.109.128>
2013-05-06 10:45:17 +0200

    Ejercicio 9

---
+++ src/paqueteHugo2/holaMundo2.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package paqueteHugo2;
+
+/**
+ *
+ * @author Administrador
+ */
+public class holaMundo2 {
+    
+    public static void main(String[] args) {    
+    System.out.println("Hola mundo");
+    }
+    
+}
